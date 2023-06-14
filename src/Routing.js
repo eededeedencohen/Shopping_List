c6c447e750df4c31e9b7eb94512d4ebde7fab84c
Eden Cohen <eededeedencohen@gmail.com>
2023-06-14 18:26:37 +0300

    14.6

--- src/Routing.js
+++ src/Routing.js
@@ -3,9 +3,6 @@ import Home from "./pages/Home";
 import PriceList from "./components/PriceList/PriceList";
 import Cart from "./components/Cart/Cart";
 import ImageParser from "./components/ImageParser/ImageParser";
-import VoiceAssistant from "./components/VoiceAssistant/VoiceAssistant";
-import Rachel from "./components/Rachel/Rachel"; // Changed this line
-import AudioUploader from "./components/Recorder/Recorder";
 import UploadRecord from "./components/UploadRecord/UploadRecord";
 import AudioRecorder from "./components/UploadRecord/AudioRecorder";
 
@@ -16,9 +13,6 @@ function Routing() {
       <Route path="/priceList/:barcode" element={<PriceList />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/image-parser" element={<ImageParser />} />
-      <Route path="/voice-assistant" element={<VoiceAssistant />} />
-      <Route path="/rachel" element={<Rachel />} /> 
-      <Route path="/audio-uploader" element={<AudioUploader />} />
       <Route path="/upload-record" element={<UploadRecord />} />
       <Route path="/audio-recorder" element={<AudioRecorder />} />
     </Routes>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,17 @@
 import React, { useEffect } from "react";
 import { useCart } from "../../context/CartContext";
 import "./Cart.css";
+import ShaareyRevahaLogo from "./SuperMarketsLogo/שערי-רווחה.jpg";
+
+const getLogoBySupermarket = (supermarketName) => {
+  // spep 1: save new name of the supermarket with - instead of space
+  const supermarketNameWithDash = supermarketName.replace(" ", "-");
+
+  const logoPath =
+    __dirname + "SupermarketLogos/" + supermarketNameWithDash + ".png";
+  console.log(logoPath);
+  return logoPath;
+};
 
 export default function Cart() {
   const { cart, loadCart } = useCart();
@@ -19,7 +30,7 @@ export default function Cart() {
       </div>
     );
   }
-  
+
   if (cart) {
     console.log(cart);
     // // print the type of cart
@@ -32,6 +43,7 @@ export default function Cart() {
       const totalPrice = cartData.data.totalPrice;
       const productsWithPrices = cartData.data.productsWithPrices;
       console.log(supermarket);
+      console.log(getLogoBySupermarket(supermarket.name));
       console.log(totalPrice);
       // for product in productsWithPrices: print product name and price
       for (let i = 0; i < productsWithPrices.length; i++) {
@@ -39,7 +51,6 @@ export default function Cart() {
         console.log(productsWithPrices[i].amount);
         console.log(productsWithPrices[i].totalPrice);
       }
-
     } catch (err) {
       console.error("Failed to parse cart data:", err);
       cartData = null;
@@ -48,12 +59,41 @@ export default function Cart() {
 
   return (
     <div className="cart">
-      <h1>Cart</h1>
-      {/* Add code to display cart data here. */}
-      {/* For example, display total price: */}
-      {cartData && <p>Total price: {cartData.data.totalPrice}</p>}
+      <div className="supermarket">
+        <div className="supermarket-title">
+          <h3>הסופרמרקט הכי משתלם לעגלה שלך </h3>
+        </div>
+        <div className="supermarket-logo">
+          <img src={ShaareyRevahaLogo} alt="Shaarey Revaha Logo" />
+        </div>
+        <div className="supermarket-address">
+          <div className="supermarket-address__city">
+            {cartData.data.supermarket.city}
+          </div>
+          <div className="supermarket-Street__street">
+            ,{cartData.data.supermarket.address}
+          </div>
+        </div>
+        <hr className="line" />
+      </div>
+      <div className="total-price">
+        <div className="total-price__title">
+          <h1>סכום סולל של העגלה שלך</h1>
+        </div>
+        <div className="total-price__price">
+          {cartData && <h1>{cartData.data.totalPrice}₪</h1>}
+        </div>
+      </div>
+      <div className="products">
+        {cartData &&
+          cartData.data.productsWithPrices.map((item, index) => (
+            <div key={index} className="product-item">
+              <h4>{item.product.name}</h4>
+              <p>Amount: {item.amount}</p>
+              <p>Total Price: {item.totalPrice}₪</p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
-
-
