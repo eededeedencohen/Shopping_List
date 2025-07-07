3fdbe7688ad355463db933ea1e6a16ae977770ce
Eden Cohen <eededeedencohen@gmail.com>
2025-07-07 13:46:46 +0300

    feat(cart-opt): add a new modal for supermarkets settings

--- src/components/CartOptimization/OptimalCartsSettings/OptimalCartsSettings.js
+++ src/components/CartOptimization/OptimalCartsSettings/OptimalCartsSettings.js
@@ -2,7 +2,9 @@ import React from "react";
 import ProductsGeneralSettings from "./ProductsGeneralSettings";
 import CalculationOptimalCarts from "./CalculationOptimalCarts";
 import SupermarketsFilter from "./SupermarketsFilter";
+import SupermarketsFilterModal from "./SupermarketsFilterModal";
 import SettingsIcon from "./settings.svg";
+import { ReactComponent as FilterIcon } from "../BrandsFilter/filter.svg";
 
 // Correct import for useNavigate
 import { useNavigate } from "react-router-dom";
@@ -13,12 +15,17 @@ import { useState } from "react";
 const OptimalCartsSettings = () => {
   const navigate = useNavigate();
   const [isAnimating, setIsAnimating] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleIconClick = () => {
     setIsAnimating(true);
     setTimeout(() => {
       navigate("/products-settings");
-    }, 800); // Navigate after 3 seconds
+    }, 800);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
   };
 
   return (
@@ -32,6 +39,19 @@ const OptimalCartsSettings = () => {
         <div className="products-settings-label">מעבר להגדרות מוצרים</div>
       </div>
       <ProductsGeneralSettings />
+      <div
+        className="supermarkets-filter-button"
+        onClick={() => setIsModalOpen(true)}
+      >
+        <div className="supermarkets-filter-icon">
+          <FilterIcon />
+        </div>
+        <div className="supermarkets-filter-label">סינון סופרמרקטים</div>
+      </div>
+      <SupermarketsFilterModal
+        isOpen={isModalOpen}
+        onClose={handleModalClose}
+      ></SupermarketsFilterModal>
       <SupermarketsFilter />
       <CalculationOptimalCarts />
     </div>
---
+++ src/components/CartOptimization/OptimalCartsSettings/SupermarketsFilterModal.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./SupermarketsFilterModal.css";
+
+const SupermarketsFilterModal = ({ isOpen, children, onClose }) => {
+  const [isRendered, setIsRendered] = useState(false);
+  const [modalStyle, setModalStyle] = useState("slide-left 0.5s ease");
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsRendered(true); // Ensure modal is rendered when it should be open
+      setModalStyle("slide-left 0.5s ease"); // Set the opening animation
+    } else if (!isOpen && isRendered) {
+      setModalStyle("slide-right 0.5s ease"); // Set the closing animation
+      setTimeout(() => {
+        setIsRendered(false); // Remove the modal from the DOM after the animation
+      }, 500); // This timeout should match the duration of your animation
+    }
+  }, [isOpen, isRendered]);
+
+  const handleOverlayClick = (event) => {
+    if (event.target.className === "modal-overlay") {
+      onClose();
+    }
+  };
+
+  // Only render the modal if it's supposed to be open or in the process of closing
+  if (!isRendered) {
+    return null;
+  }
+
+  return ReactDOM.createPortal(
+    <div className="modal-overlay" onClick={handleOverlayClick}>
+      <div className="smf_modal-window" style={{ animation: modalStyle }}>
+        {children}
+      </div>
+    </div>,
+    document.getElementById("modal-root")
+  );
+};
+
+export default SupermarketsFilterModal;
